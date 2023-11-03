b5a524ea7d51554d902bf983653b80d3bad35dd6
Daniel Skrypnik <forte201253@gmail.com>
2023-11-03 12:34:06 +0200

    upgrade docusaurus to 3.0

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -1,48 +1,50 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/palenight");
+const { themes } = require('prism-react-renderer');
+
+const darkCodeTheme = themes.palenight;
+const lightCodeTheme = themes.github;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Driver App",
-  tagline: "Dinosaurs are cool",
-  favicon: "img/favicon.ico",
+  title: 'Driver App',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-test-site.com",
+  url: 'https://your-docusaurus-test-site.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: "facebook", // Usually your GitHub org/user name.
-  projectName: "docusaurus", // Usually your repo name.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "warn",
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: 'en',
+    locales: ['en'],
   },
 
   presets: [
     [
-      "classic",
+      'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: "/",
-          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/fermersky/node-ddd-procedural/blob/main/drivers-app-docs/",
+          editUrl: 'https://github.com/fermersky/node-ddd-procedural/blob/main/drivers-app-docs/',
         },
         // blog: {
         //   showReadingTime: true,
@@ -53,7 +55,7 @@ const config = {
         // },
         blog: false,
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
       }),
     ],
@@ -63,28 +65,28 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: 'img/docusaurus-social-card.jpg',
       colorMode: {
         respectPrefersColorScheme: true,
       },
       navbar: {
-        title: "Driver App",
+        title: 'Driver App',
         // logo: {
         //   alt: 'My Site Logo',
         //   src: 'img/logo.svg',
         // },
         items: [
           {
-            type: "docSidebar",
-            sidebarId: "mySidebar",
-            position: "left",
-            label: "API docs",
+            type: 'docSidebar',
+            sidebarId: 'mySidebar',
+            position: 'left',
+            label: 'API docs',
           },
           // { to: '/blog', label: 'Blog', position: 'left' },
           {
-            href: "https://github.com/fermersky/node-ddd-procedural",
-            label: "GitHub",
-            position: "right",
+            href: 'https://github.com/fermersky/node-ddd-procedural',
+            label: 'GitHub',
+            position: 'right',
           },
         ],
       },
